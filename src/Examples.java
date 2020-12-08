ddc9f13b348d97af6702ea1f0e25c0be72cd43b6
vuongdbrandon <57996169+vuongdbrandon@users.noreply.github.com>
2020-12-08 17:12:04 -0500

    examples to test code

---
+++ src/Examples.java
@@ -0,0 +1,72 @@
+public class Examples {
+    public static void main(String[] args) {
+
+        Exception e2 = new CandidateExistsException("gompei");
+        Exception e3 = new DuplicateVotesException("gompei");
+        ElectionData ED = new ElectionData();
+
+        //adding an existing candidate
+        try {
+            ED.addCandidate("Gompei");
+        }
+        catch(CandidateExistsException e) {
+            System.out.println(e.getMessage());
+        }
+
+        //adding a new candidate
+        try {
+            ED.addCandidate("Falco");
+            System.out.println(ED.ballot);
+        }
+        catch(CandidateExistsException e) {
+            System.out.println(e.getMessage());
+        }
+
+        //one candidate does not exists
+        try {
+            ED.processVote("Gompei", "Fox", "Falco");
+        }
+        catch(UnknownCandidateException e) {
+            System.out.println(e.getMessage());
+        }
+        catch(DuplicateVotesException e) {
+            System.out.println(e.getMessage());
+        }
+
+        //voting for two of the same candidates
+        try {
+            ED.processVote("Gompei", "Gompei", "Falco");
+        }
+        catch(UnknownCandidateException e) {
+            System.out.println(e.getMessage());
+        }
+        catch(DuplicateVotesException e) {
+            System.out.println(e.getMessage());
+        }
+
+        //proper vote
+        try {
+            ED.processVote("Gompei", "Husky", "Falco");
+            System.out.print(ED.votesHash + "\n");
+        }
+        catch(UnknownCandidateException e) {
+            System.out.println(e.getMessage());
+        }
+        catch(DuplicateVotesException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            ED.processVote("Husky", "Gompei", "Falco");
+            System.out.print(ED.votesHash + "\n");
+        }
+        catch(UnknownCandidateException e) {
+            System.out.println(e.getMessage());
+        }
+        catch(DuplicateVotesException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+}
