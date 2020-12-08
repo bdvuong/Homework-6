b1f1ae4e75080634d1f857f6e33689cb039d724b
vuongdbrandon <57996169+vuongdbrandon@users.noreply.github.com>
2020-12-08 10:35:51 -0500

    constructor added

--- src/CandidateExistsException.java
+++ src/CandidateExistsException.java
@@ -1,2 +1,11 @@
+/**
+ * thrown if a candidate already exists this exception is thrown
+ */
 public class CandidateExistsException extends Exception {
+
+    String candidate;
+
+    CandidateExistsException(String name) {
+        this.candidate = name;
+    }
 }
