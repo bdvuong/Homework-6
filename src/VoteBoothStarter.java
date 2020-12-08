b7eaf799d4d557d9f975f7b2fa841bb8a7f7431b
vuongdbrandon <57996169+vuongdbrandon@users.noreply.github.com>
2020-12-08 10:03:25 -0500

    hashMap data structure added and processVote method added

--- src/VoteBoothStarter.java
+++ src/VoteBoothStarter.java
@@ -1,9 +1,13 @@
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 class ElectionData {
   LinkedList<String> ballot = new LinkedList<String>();
   LinkedList<String> votes = new LinkedList<String>();
+
+  HashMap<String, LinkedList<String>> votesHash = new HashMap<>();
+
   Scanner keyboard = new Scanner(System.in);
   
   ElectionData() {
@@ -34,4 +38,24 @@ class ElectionData {
     }
     return numvotes;
     }
+
+  /**
+   * takes three strings as a choice based vote
+   * @param first the voter's first choice vote
+   * @param second the voter's second choice vote
+   * @param third the voter's third choice vote
+   */
+    public void processVote(String first, String second, String third) {
+      LinkedList<String> firstList = this.votesHash.get("First");
+      firstList.add(first);
+      LinkedList<String> secondList = this.votesHash.get("Second");
+      secondList.add(second);
+      LinkedList<String> thirdList = this.votesHash.get("Third");
+      thirdList.add(third);
+
+      votesHash.put("First", firstList);
+      votesHash.put("Second", secondList);
+      votesHash.put("Third", thirdList);
+    }
+
   }
